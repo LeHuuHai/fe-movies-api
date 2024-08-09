b109a36fcc77e046e8c25b6c4c7600a6798f6543
hari <le31052003@gmail.com>
2024-08-09 10:37:36 +0700

    push src code

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+import { useEffect, useState } from 'react';
+import './App.css';
+import api from "./api/axiosConfig"
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+
+
+function App() {
+
+  const [movies, setMovies] = useState();
+
+  const getMovies = async ()=>{
+    try {
+      const response = await api.get("/api/v1/movies");
+      // console.log(response.data);
+      setMovies(response.data);
+    } catch (err) {
+      console.error(err.message);
+    }
+    
+  }
+
+  useEffect(()=>{
+    getMovies();
+  },[])
+
+  return (
+    <div className="App">
+      <Header/>
+      <Routes> 
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home movies={movies}/>}></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
