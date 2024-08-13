9a53efd39abaacbc293254aeeb48b8c02e77fb35
hari <le31052003@gmail.com>
2024-08-13 18:06:32 +0700

    change baseURL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://pimg-v1-0.onrender.com',
     headers: {"ngrok-skip-browse-warning": "true"}
 });
